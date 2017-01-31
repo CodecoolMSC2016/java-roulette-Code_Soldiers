ae86b9e5b1dfa1ff5d666ec94226b16a66e54ae5
László Székely-Tóth <notisracer@gmail.com>
2017-01-31 15:14:21 +0100

    Simulation.generateData() done

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public Simulation generateSimulation(int round) {
-
+    return null;
   }
 
   public static void main(String[] args) {
--- Simulation.java
+++ Simulation.java
@@ -1,9 +1,17 @@
-public class Simulation {
-  public void generateData() {
+import java.util.ArrayList;
+import java.util.Random;
 
+
+public class Simulation {
+  String data;
+  private static String colors = "grbrbrbrbrbbr" + "brbrbrrbrbrb" + "rbrbbrbrbrbr";
+  public void generateData() {
+    int random = new Random().ints(1, 0, 37).findFirst().getAsInt();
+    data = "" + colors.charAt(random) + String.valueOf(random);
   }
 
   public Simulation load() {
 
+    return null;
   }
 }
