f200b133c7c29668dddf3244a4c180a15cab374e
ThomasKenyeres <kenyerest97@gmail.com>
2017-01-31 14:21:43 +0100

    Logger log TimeStamp implemented

--- Logger.java
+++ Logger.java
@@ -1,5 +1,20 @@
-public class Logger {
-  public void log(String type, String message) {
+import java.util.*;
+import java.text.*;
 
+public class Logger {
+  public void log(String type, String message){
+    switch(type){
+      case "TimeStamp":
+        String text = getDate() + " " + type + " " + message;
+        System.out.println(text);
+      default:
+        System.out.println(message);
+    }
+  }
+  
+  private String getDate(){
+    DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+    Date today = Calendar.getInstance().getTime();
+    return df.format(today);
   }
 }
