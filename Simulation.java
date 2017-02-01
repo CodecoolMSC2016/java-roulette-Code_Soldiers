fe2b52fa7e0082c3f5b9f650644e5588cc9e845a
László Székely-Tóth <notisracer@gmail.com>
2017-02-01 10:55:19 +0100

    Simulation generateData fixed

--- Simulation.java
+++ Simulation.java
@@ -1,13 +1,24 @@
 import java.util.ArrayList;
-import java.util.Random;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.FileOutputStream;
 
 
 public class Simulation {
   String data;
   private static String colors = "grbrbrbrbrbbr" + "brbrbrrbrbrb" + "rbrbbrbrbrbr";
-  public void generateData() {
-    int random = new Random().ints(1, 0, 37).findFirst().getAsInt();
-    data = "" + colors.charAt(random) + String.valueOf(random);
+  public Simulation(int number) {
+    data = "" + colors.charAt(number) + String.valueOf(number);
+  }
+  public void generateData() throws IOException {
+    PrintWriter fw;
+    try {
+      fw = new PrintWriter(new FileOutputStream("Simulation.csv", true));
+      fw.println(data);
+      fw.close();
+    } catch(IOException e) {
+      // "Simulation.csv", "UTF-8"
+    }
   }
 
   public Simulation load() {
