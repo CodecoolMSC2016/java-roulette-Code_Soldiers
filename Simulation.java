4339f7c3b2d61ff23af09d7d087135e5bf87d269
László Székely-Tóth <notisracer@gmail.com>
2017-02-01 11:58:21 +0100

    fucking simulation finnaly fixed i guess

--- Simulation.java
+++ Simulation.java
@@ -2,27 +2,47 @@ import java.util.ArrayList;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.FileOutputStream;
+import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 
 public class Simulation {
-  String data;
+  ArrayList<String> data;
   private static String colors = "grbrbrbrbrbbr" + "brbrbrrbrbrb" + "rbrbbrbrbrbr";
-  public Simulation(int number) {
-    data = "" + colors.charAt(number) + String.valueOf(number);
+  public Simulation() {
+
   }
+
   public void generateData() throws IOException {
+    int r = new Random().ints(1, 0, 37).findFirst().getAsInt();
+    String rec = "" + colors.charAt(r) + String.valueOf(r);
+    data.add(rec);
+    writeCsv(rec);
+  }
+
+  public Simulation load() {
+    BufferedReader br;
+    String line;
+    try {
+      br = new BufferedReader(new FileReader("Simulation.csv"));
+      while((line = br.readLine()) != null) {
+        data.add(line);
+      }
+    }
+    catch (IOException e) {
+
+    }
+    return null;
+  }
+
+  private void writeCsv(String txt)
+  {
     PrintWriter fw;
     try {
       fw = new PrintWriter(new FileOutputStream("Simulation.csv", true));
       fw.println(data);
       fw.close();
-    } catch(IOException e) {
-      // "Simulation.csv", "UTF-8"
-    }
-  }
-
-  public Simulation load() {
-      
-    return null;
+    } catch(IOException e) { }
   }
 }
