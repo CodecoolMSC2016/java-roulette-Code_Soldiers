5542f989a45d831ff8b8d17915cddd5a50ae8f85
ThomasKenyeres <kenyerest97@gmail.com>
2017-02-01 14:12:39 +0100

    checkEvenOdd corrected

--- Simulator.java
+++ Simulator.java
@@ -12,29 +12,15 @@ public class Simulator {
     return result;
   }
   public HashMap<String, Integer> checkColor(ArrayList<String> datas) {
-    int green = 0, black = 0, red = 0;
-    HashMap<String, Integer> result = null;
-    for(String data: datas) {
-      if(data.substring(0, 1).equals("g")) {
-        green += 1;
-      } else if(data.substring(0, 1).equals("b")) {
-        black += 1;
-      } else {
-        red += 1;
-      }
-    }
-    result.put("Green", green);
-    result.put("Black", black);
-    result.put("Red", red);
-    return result;
+    return null;
   }
   public HashMap<String, Integer> checkEvenOdd(ArrayList<String> datas) {
     int even = 0;
     int odd = 0;
-    Hashmap<String, Integer> map = new Hashmap<String, Integer>();
+    HashMap<String, Integer> map = new HashMap<String, Integer>();
 
     for(int i = 0; i < datas.size(); i++) {
-      int currentNum = datas[i].substring(1);
+      int currentNum = datas.get(i).substring(1);
       if(currentNum % 2 == 0) {
         even += 1;
       }
