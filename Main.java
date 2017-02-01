e38cd6bf3cc01f148a4158c754d2f5a3f85751ef
László Székely-Tóth <notisracer@gmail.com>
2017-02-01 12:22:41 +0100

    implemented argument check in main, throws IllegalArgumentException if no argument

--- Main.java
+++ Main.java
@@ -1,16 +1,24 @@
 public class Main {
   public static Simulation generateSimulation(int round) {
     Simulation simulation = new Simulation();
-    simulation.load();
     for(int i = 0; i < round; i++) {
       simulation.generateData();
     }
     return simulation;
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IllegalArgumentException {
     Logger logger = null;
-    Simulation simulation = generateSimulation(Integer.parseInt(args[0]));
-    Simulator simulator = new Simulator(simulation, logger);
+    Simulation simulation = null;
+    Simulator simulator = null;
+    if(args.length == 1)
+    {
+      simulation = generateSimulation(Integer.parseInt(args[0]));
+      simulator = new Simulator(simulation, logger);
+    }
+    else
+      throw new IllegalArgumentException();
+    // simulation.load();
+
   }
 }
