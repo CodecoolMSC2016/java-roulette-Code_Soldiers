c0d5df20754574d7b2eb7c0fe79619bfde56b9bd
David Szilagyi <szilagyidav@gmail.com>
2017-02-01 19:32:22 +0100

    Refactor and used LinkedHashMap instead of HashMap

--- Simulator.java
+++ Simulator.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 public class Simulator {
   Simulation simulation;
   Logger logger;
@@ -10,47 +10,42 @@ public class Simulator {
   @SuppressWarnings("rawtypes")
   public Result run() {
     Result result = new Result();
-    result.setResults(new HashMap[] {checkColor(simulation.getData()), checkEvenOdd(simulation.getData()),
-                                     checkNumbers(simulation.getData()), checkRow(simulation.getData()),
-                                     checkRange(simulation.getData(), 1, 18), checkRange(simulation.getData(), 19, 36),
-                                     checkRange(simulation.getData(), 1, 12), checkRange(simulation.getData(), 13, 24),
-                                     checkRange(simulation.getData(), 25, 36)});
+    String[] ranges = new String[] {"1-18", "19-36", "1-12", "13-24", "25-36"};
+    result.setResults(new LinkedHashMap[] {checkColor(simulation.getData()), checkEvenOdd(simulation.getData()),
+                                     checkNumbers(simulation.getData()), checkRow(simulation.getData())});
+    for(String range: ranges) {
+      result.setResults(new LinkedHashMap[] {checkRange(simulation.getData(), range)});
+    }
     return result;
   }
-  public HashMap<String, Integer> checkColor(ArrayList<String> datas) {
+  public LinkedHashMap<String, Integer> checkColor(ArrayList<String> datas) {
     int black = 0, red = 0;
-    HashMap<String, Integer> result = new HashMap<String, Integer>();
+    LinkedHashMap<String, Integer> result = new LinkedHashMap<String, Integer>();
     for(String data: datas) {
       if(data.substring(0, 1).equals("b")) {
-        black += 1;
+        result.put("Black", ++black);
       } else {
-        red += 1;
+        result.put("Red", ++red);
       }
     }
-    result.put("Black", black);
-    result.put("Red", red);
     return result;
   }
-  public HashMap<String, Integer> checkEvenOdd(ArrayList<String> datas) {
-    int even = 0;
-    int odd = 0;
-    HashMap<String, Integer> map = new HashMap<String, Integer>();
-
+  public LinkedHashMap<String, Integer> checkEvenOdd(ArrayList<String> datas) {
+    int even = 0, odd = 0;
+    LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
     for(int i = 0; i < datas.size(); i++) {
       int currentNum = Integer.parseInt(datas.get(i).substring(1));
       if(currentNum % 2 == 0) {
-        even += 1;
+        map.put("Even", ++even);
       }
       else {
-          odd += 1;
+        map.put("Odd", ++odd);
       }
     }
-    map.put("Even", even);
-    map.put("Odd", odd);
     return map;
   }
-  public HashMap<String, Integer> checkNumbers(ArrayList<String> dataList) {
-    HashMap<String, Integer> map = new HashMap<String, Integer>();
+  public LinkedHashMap<String, Integer> checkNumbers(ArrayList<String> dataList) {
+    LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
     Integer[] data = new Integer[dataList.size()];
     for(int i = 0; i < dataList.size(); i++) {
       data[i] = Integer.parseInt(dataList.get(i).substring(1));
@@ -66,35 +61,32 @@ public class Simulator {
     }
     return map;
   }
-  public HashMap<String, Integer> checkRow(ArrayList<String> datas) {
-    HashMap<String, Integer> result = new HashMap<String, Integer>();
+  public LinkedHashMap<String, Integer> checkRow(ArrayList<String> datas) {
+    LinkedHashMap<String, Integer> result = new LinkedHashMap<String, Integer>();
     int first = 0, second = 0, third = 0;
     for(String data: datas) {
       if(Integer.parseInt(data.substring(1)) != 0) {
         if(Integer.parseInt(data.substring(1)) % 3 == 1) {
-          first += 1;
+          result.put("First", ++first);
         } else if (Integer.parseInt(data.substring(1)) % 3 == 2) {
-          second += 1;
+          result.put("Second", ++second);
         } else {
-          third += 1;
+          result.put("Third", ++third);
         }
       }
     }
-    result.put("First", first);
-    result.put("Second", second);
-    result.put("Third", third);
     return result;
   }
-  public HashMap<String, Integer> checkRange(ArrayList<String> datas, int start, int stop) {
-    String range = Integer.toString(start) + "-" + Integer.toString(stop);
+  public LinkedHashMap<String, Integer> checkRange(ArrayList<String> datas, String range) {
+    int start = Integer.parseInt(range.substring(0, range.indexOf("-")));
+    int stop = Integer.parseInt(range.substring(range.indexOf("-") + 1));
     int counter = 0;
-    HashMap<String, Integer> result = new HashMap<String, Integer>();
+    LinkedHashMap<String, Integer> result = new LinkedHashMap<String, Integer>();
     for(String data: datas) {
       if(start <= Integer.parseInt(data.substring(1)) && Integer.parseInt(data.substring(1)) <= stop) {
-        counter += 1;
+        result.put(range, ++counter);
       }
     }
-    result.put(range, counter);
     return result;
   }
 }
