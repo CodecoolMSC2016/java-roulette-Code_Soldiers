81b11ac6deb4c04bc3cc8850947900bc1700692c
David Szilagyi <szilagyidav@gmail.com>
2017-02-01 21:32:37 +0100

    getter definied

--- Result.java
+++ Result.java
@@ -1,6 +1,8 @@
 import java.util.LinkedHashMap;
 public class Result {
   protected LinkedHashMap<String, Integer> result;
+  Simulation simulation;
+  Simulator simulator;
   public Result() {
     result = new LinkedHashMap<String, Integer>();
   }
@@ -10,7 +12,25 @@ public class Result {
       result.putAll(hashmap);
     }
   }
-  public LinkedHashMap<String, Integer> getResults() {
+  public void setResults(LinkedHashMap<String, Integer> resultsMap) {
+    result = resultsMap;
+  }
+  public LinkedHashMap<String, Integer> getFullResults() {
       return result;
+  }
+  public LinkedHashMap<String, Integer> getColor() {
+    return simulator.checkColor(simulation.getData());
+  }
+  public LinkedHashMap<String, Integer> getEvenOdd() {
+    return simulator.checkEvenOdd(simulation.getData());
+  }
+  public LinkedHashMap<String, Integer> getNumbers() {
+    return simulator.checkNumbers(simulation.getData());
+  }
+  public LinkedHashMap<String, Integer> getRow() {
+    return simulator.checkRow(simulation.getData());
+  }
+  public LinkedHashMap<String, Integer> getRange() {
+    return simulator.checkRange(simulation.getData(), simulator.ranges);
   }
 }
