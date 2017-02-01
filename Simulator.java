ed456f2bd083c2ed3e46bbf496f71714653c6b04
David Szilagyi <szilagyidav@gmail.com>
2017-02-01 13:28:14 +0100

    Simulation skeleton done

--- Simulator.java
+++ Simulator.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 public class Simulator {
+  Simulation simulation;
+  Logger logger;
   public Simulator(Simulation simulation, Logger logger) {
-
+    this.simulation = simulation;
+    this.logger = logger;
   }
-
   public Result run() {
     return new Result(new Simulation());
+  }
+  public HashMap<String, Double> checkColor(ArrayList<String> datas) {
+   return null;
+  }
+  public HashMap<String, Double> checkEvenOdd(ArrayList<String> datas) {
+   return null;
+  }
+  public HashMap<String, Double> checkNumbers(ArrayList<String> datas) {
+   return null;
+  }
+  public HashMap<String, Double> checkRow(ArrayList<String> datas) {
+   return null;
+  }
+  public HashMap<String, Double> checkRange(ArrayList<String> datas) {
+   return null;
   }
 }
